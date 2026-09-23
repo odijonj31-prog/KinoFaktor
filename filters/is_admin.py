5f4cedfe18dad20b006b95a5a24c65dda1df3bbe
odijonj31-prog <odijonj31@gmail.com>
2026-09-23 07:46:57 +0500

    Update is_admin.py

--- filters/is_admin.py
+++ filters/is_admin.py
@@ -1,9 +1,11 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
-from config import SUPER_ADMIN_ID
+from config import SUPER_ADMIN_IDS
 from database.requests import is_admin as db_is_admin
 
 
 class IsAdmin(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        return await db_is_admin(message.from_user.id, SUPER_ADMIN_ID)
+        if message.from_user.id in SUPER_ADMIN_IDS:
+            return True
+        return await db_is_admin(message.from_user.id, 0)
